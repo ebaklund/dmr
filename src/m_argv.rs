50c7c542c16fc71da0535f6f28abbde81f648ea5
Erik Baklund <erik@hubii.com>
2022-12-30 06:18:19 +0100

    Alternative read of response file

---
+++ src/m_argv.rs
@@ -0,0 +1,99 @@
+
+use std::iter::Iterator;
+use std::path::PathBuf;
+use std::fs::{File};
+use std::io::{prelude::*, BufReader};
+use clap::{Parser};
+
+// https://crates.io/crates/clap
+// https://github.com/clap-rs/clap
+// https://docs.rs/clap/latest/clap/all.html
+// https://docs.rs/clap/latest/clap/_derive/index.html
+// https://blog.rng0.io/compile-time-feature-flags-in-rust-why-how-when
+
+
+#[derive(Parser, Debug)]
+pub struct DmrArgs {
+   #[arg(long, num_args(1))]
+   iwad: Option<String>,
+
+   #[arg(long, num_args(1))]
+   response: Option<String>,
+}
+
+impl DmrArgs
+{
+    pub fn new() -> Result<DmrArgs, String>
+    {
+        let parse_res = <DmrArgs as clap::Parser>::try_parse();
+
+        match parse_res
+        {
+            Ok(dmr_args) => dmr_args.try_update_from_response_file(),
+            Err(err) => Err(err.to_string())
+        }
+    }
+
+    // PRIVATE HELPERS
+
+    fn try_update_from_response_file(mut self: DmrArgs) -> Result<DmrArgs, String>
+    {
+        let path_opt = self.get_response_path()?;
+
+        match path_opt
+        {
+            Some(path) =>
+            {
+                let file =  match File::open(path)
+                {
+                    Ok(file) => Ok(file),
+                    Err(str) => Err(str.to_string())
+                }?;
+
+                let reader = BufReader::new(file);
+
+                let lines = reader
+                    .lines()
+                    .filter_map(|line| line.ok())
+                    .collect::<Vec<String>>();
+
+                let words = lines
+                    .iter()
+                    .flat_map(|line| line.split_whitespace())
+                    .map(|word| word.to_string())
+                    .collect::<Vec<String>>();
+
+                match self.try_update_from(words)
+                {
+                    Ok(_) =>  Ok(self),
+                    Err(err) => Err(err.to_string())
+                }
+            },
+            None => Ok(self)
+        }
+    }
+
+    fn get_response_path(self: &Self) -> Result<Option<PathBuf>, String>
+    {
+        match &self.response
+        {
+            Some(file_name) =>
+            {
+                let path = PathBuf::from(file_name);
+
+                if !path.exists()
+                {
+                    return Err(format!("Path {} does not exist.", file_name))
+                }
+
+                if !path.is_file()
+                {
+                    return Err(format!("Path {} is not a file.", file_name))
+                }
+
+                Ok(Some(path))
+            },
+            _ => Ok(None)
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,43 @@
-extern crate libc;
+
+mod m_argv;
 
 use std::ffi::CString;
+
+extern crate libc;
 use libc::c_char;
 use libc::c_int;
 
 // https://stackoverflow.com/questions/69437925/problem-with-calling-c-function-that-receive-command-line-arguments-from-rust
 
-//#[link(name="i_main")]
 #[link(name = "dmc", kind = "static")]
 extern "C"
 {
     static mut myargc: c_int;
     static mut myargv: *const *const c_char;
-
-    fn M_FindResponseFile();
     fn D_DoomMain ();
 }
 
 fn main()
 {
-    let pinned_args = std::env::args()
-        .map(|arg| CString::new(arg).unwrap())
-        .collect::<Vec<CString>>();
-
-    let argv: Vec<*const c_char> = pinned_args.iter().map(|s| s.as_ptr()).collect();
+    let _argv = m_argv::DmrArgs::new();
 
     unsafe
     {
-        myargc = argv.len() as c_int;
-        myargv = argv.as_ptr();
+        let args: Vec<String> = std::env::args().collect();
 
-        M_FindResponseFile();
-        D_DoomMain ();
+        let pinned_args: Vec<CString> = args
+            .iter()
+            .map(|s| CString::new(s.to_string()).unwrap())
+            .collect();
+
+        let c_argv: Vec<*const c_char> = pinned_args
+            .iter()
+            .map(|s| s.as_ptr())
+            .collect();
+
+        myargc = c_argv.len() as c_int;
+        myargv = c_argv.as_ptr();
+
+        D_DoomMain();
     };
 }
