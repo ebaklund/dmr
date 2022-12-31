4aecece5a1e8e8d582183fbb2f328f6e241dfcfa
Erik Baklund <erik@hubii.com>
2022-12-31 22:51:44 +0100

    Added some vargs

--- src/m_argv.rs
+++ src/m_argv.rs
@@ -12,12 +12,98 @@ use clap::{Parser};
 // https://blog.rng0.io/compile-time-feature-flags-in-rust-why-how-when
 
 #[derive(Parser, Debug)]
-pub struct DArgv {
-   #[arg(long, required(true), num_args(1))]
-   pub iwad: String,
+pub struct DArgv
+{
+    // d_iwad
 
-   #[arg(long, num_args(1))]
-   pub response: Option<String>,
+    #[arg(long, required(true), num_args(1),
+      help("Load game from given IWAD."))]
+    pub iwad: String,
+
+    // i_musicpack
+
+    #[arg(long, num_args(1),
+      help("Dump MIDI example substitution to specified file."),
+      long_help("Read all MIDI files from loaded WAD files, dump an example substitution music config file to the specified filename and quit."))]
+    pub dumpsubstconfig: Option<String>,
+
+    // i_video
+
+    #[arg(long, num_args(1), default_value("2014"),
+      help("Sets the width of the window. Implies -window."))]
+    pub width: u32,
+
+    #[arg(long, num_args(1), default_value("2014"),
+      help("Sets the height of the window. Implies -window."))]
+    pub height: u32,
+
+    // m_argv
+
+    #[arg(long, num_args(1),
+      help("Merge command line arguments from the given response file."))]
+    pub response: Option<String>,
+
+    // m_config
+
+    #[arg(long, num_args(1),
+      help("Load main configuration from the specified file, instead of the default."))]
+    pub config: Option<String>,
+
+    #[arg(long, num_args(1),
+      help("Load extra configuration from the specified file, instead of the extra default."))]
+    pub extraconfig: Option<String>,
+
+    // w_main
+
+    #[arg(long, trailing_var_arg(true), value_delimiter(','),
+      help("Merge one or more PWAD file into the main IWAD, deutex's merge style."))]
+    pub merge: Option<Vec<String>>,
+
+    #[arg(long, trailing_var_arg(true), value_delimiter(','),
+      help("Merge one or more PWAD file into the main IWAD, NWT's merge style."))]
+    pub nwtmerge: Option<Vec<String>>,
+
+    #[arg(long, trailing_var_arg(true), value_delimiter(','),
+      help("Merge flats into the main IWAD directory, NWT's merge style."))]
+    pub nwtaf: Option<Vec<String>>,
+
+    #[arg(long, trailing_var_arg(true), value_delimiter(','),
+      help("Merge sprites into the main IWAD directory, NWT's merge style."))]
+    pub nwtas: Option<Vec<String>>,
+
+    #[arg(long, trailing_var_arg(true), value_delimiter(','),
+      help("Load the specified PWAD files."))]
+    pub file: Option<Vec<String>>,
+
+    // d_main
+
+    #[arg(long, num_args(1), default_value("1.9"), value_parser(["1.2", "1.666", "1.7", "1.8", "1.9"]),
+      help("Emulate a specific version of Doom."))]
+    pub gameversion: String,
+
+    #[arg(long, num_args(1),
+      help("Dump IWAD file merged with PWADs into given file. See: --merge and --nwtmerge"))]
+    pub mergedump: Option<String>,
+
+    #[arg(long, num_args(1),
+      help("Play back the given demo file (*.lmp)."))]
+    pub playdemo: Option<String>,
+
+    #[arg(long, num_args(1),
+      help("Play back the given demo file while tracking framerate (*.lmp)."))]
+    pub timedemo: Option<String>,
+
+    #[arg(long, num_args(1), value_parser(0..=5),
+      help("Set game skill (1: easiest - 5: hardest). 0 disables all monsters."))]
+    pub skill: Option<u32>,
+
+    #[arg(long, num_args(1), value_parser(1..=4),
+      help("Set game episode to start on (1-4)."))]
+    pub episode: Option<u32>,
+
+    #[arg(long, num_args(1), default_value("20"),
+      help("Set level time limit for multiplayer games in minutes."))]
+    pub timer: Option<u32>,
 }
 
 impl DArgv
